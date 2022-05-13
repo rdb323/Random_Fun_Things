36f5b9f6fcf8cc7bb08062fff02892311d571176
rdb323 <barthr@seattleu.edu>
2022-05-13 13:34:03 -0700

    Created a folder for the RDT3 files and uploaded a *slightly* imporved alarm file

--- alarm.py
+++ alarm.py
@@ -5,14 +5,29 @@ from playsound import playsound
 def pingtest():
     has_internet = False
     while not has_internet:
+        time.sleep(2)
         response = os.system("ping www.google.com")
 
         if response == 0:
             has_internet = True
-        time.sleep(1)
+        
     playsound('C:/Users/Ryan/Python_codebase/Internet_Alarm/FFVII.mp3')
-    print("You now have internet, yay!")
+    print("\r\nYou now have internet, yay!")
 
 
 if __name__ == '__main__':
-    pingtest()
+    #pingtest()
+    
+    a = 512
+    a = a.to_bytes(2,'big')
+    print(a)
+    b = 64
+    b = b.to_bytes(2,'big')
+    print(b)
+    
+    for i in range(len(a)):
+        print(format(a[i], "08b"))
+
+    for i in range(len(b)):
+        print(format(b[i], "08b"))
+    
